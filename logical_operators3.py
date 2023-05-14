b9139801fd4fecfad6afe9a9d88ea64e5e137663
Abraham John <jabrahamjohns@gmail.com>
2023-05-15 02:45:41 +0300

    logical operators using NOT

---
+++ logical_operators3.py
@@ -0,0 +1,23 @@
+# Test Example 1:
+
+x = 2*3 > 6
+print("The value of x is:")
+print(x)
+
+print("")  # Prints a blank line
+
+print("The inverse value of x is:")
+print(not x)
+
+# What happens when you negate a False statement?
+
+
+today = "Monday"
+print(not today == "Tuesday")
+
+
+# The "today" variable states today is Monday. This makes the comparison
+# "today == Tuesday" False. The logical operator "not" inverts the False
+# result to become True. In other words, this expression asks if it is
+# false that today is not Tuesday. More succinctly, "not False" means
+# True."
