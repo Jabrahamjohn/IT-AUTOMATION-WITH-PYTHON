daab4a264d5a1b26cce044d5597b0824b81ae460
Abraham John <jabrahamjohns@gmail.com>
2023-05-15 02:41:13 +0300

    logical operators using OR

---
+++ logical_operators2.py
@@ -0,0 +1,13 @@
+# True or True returns True
+print((15/3 < 2+4) or (0 >= 6-7))
+
+# False or True returns True
+print(country == "New York City" or city == "New York City")
+
+# True or False returns True
+print(16 <= 4**2 or 9**(0.5) != 3)
+
+
+# False or False returns False
+print("B_name" > "C_name" or "B_name" < "A_name”)
+
