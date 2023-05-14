7a0f676d36ae6348d95782f2450e5a187ee33c2d
Abraham John <jabrahamjohns@gmail.com>
2023-05-15 02:33:15 +0300

    adding a logical operators with and

--- logical_operators.py
+++ logical_operators.py
@@ -1,1 +1,2 @@
-print((6*3 >= 18) and (9+9 <= 36/2))
+print((6*3 >= 18) and (9+9 <= 36/2)) #prints a true statement
+print("Nairobi" < "Milan" and "Nairobi" > "Hanoi") #this prints a false statement
