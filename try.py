9f8555078a8a08a25419a44336d468d5c1d78f3b
Abraham John <jabrahamjohns@gmail.com>
2023-05-15 05:22:02 +0300

    tried something new

---
+++ try.py
@@ -0,0 +1,10 @@
+def interger(number):
+    if number > 11:
+        print(0)
+    elif number != 10:
+        print(1)
+    elif number >= 20 or number < 12:
+        print(2)
+    else:
+        print(3)
+print(interger(10))
