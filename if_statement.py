dfc9cdc8abc14a3ed6a721194ff16f499e261695
Abraham John <jabrahamjohns@gmail.com>
2023-05-15 03:21:40 +0300

    branching in python with if statements

---
+++ if_statement.py
@@ -0,0 +1,5 @@
+def hint_usename(username):
+    if len(username) < 3:
+        print("invalid username. must be atleast 3 characters long")
+    else:
+        print("valid username")
---
+++ if_statement2.py
@@ -0,0 +1,4 @@
+def is_even(number):
+    if number % 2 == 0:
+        return True
+    return False
