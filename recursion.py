aeea5352e37ceccc7279835c3fbd91895bf85a5d
Abraham John <jabrahamjohns@gmail.com>
2023-05-17 19:59:54 +0300

    recursivve functions

---
+++ recursion.py
@@ -0,0 +1,6 @@
+# A recursive function will usually have this structure:
+
+# def recursive_function(parameters):
+#    if base_case_condition(parameters):
+#       return base_case_value
+#    recursive_function(modified_parameters)
